fcdb1af69c8fba4a6633f3c00cfae73f85cf543b
Vladimir Egorov <24740397+Vladimire87@users.noreply.github.com>
2023-07-19 13:26:17 +0400

    add model save

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,5 +1,23 @@
 class ContactsController < ApplicationController
-  def new; end
+  def new
+    @contact = Contact.new
+  end
 
-  def create; end
+  def create
+    @contact = Contact.new(contact_params)
+
+    if @contact.save
+      # Redirect to the contacts page or a thank-you page after successful submission
+      redirect_to contacts_path, notice: 'Thank you for your message!'
+    else
+      # If the contact record couldn't be saved, re-render the new form with the errors
+      render :new
+    end
+  end
+
+  private
+
+  def contact_params
+    params.require(:contact).permit(:email, :message)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  root 'home#index'
-  resource :contacts, only: [:new, :create]
+  get 'home/index'
+  resources :contacts, only: %i[new create]
 
   resources :articles
 end
